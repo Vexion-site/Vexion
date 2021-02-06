623a3daf9a303583ab71846882d3c6def97ef119
Editor <lordvex@keemail.me>
2021-02-06 20:30:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 console.log("LOADED");
+
+let map = L.map('mapid').setView([45,20],13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -6,5 +8,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 		id: 'mapbox/streets-v11',
 		tileSize: 512,
 		zoomOffset: -1
-	}).addTo(mymap);
-let map = L.map('mapid').setView([45,20],13);
+	}).addTo(map);
