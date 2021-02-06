48ca6db083f2c21160d02056d09216cfed960b46
Editor <lordvex@keemail.me>
2021-02-06 20:50:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("LOADED");
 
-let map = L.map('mapid').setView([45,20],13);
+let map = L.map('mapid').setView([44.799722,20.405278],13);
 
 let Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -20,6 +20,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // map.addLayer(Stamen_Toner);
 
 
-let marker = L.marker([45, 20], {title: 'VEXION LOCATION'});
+let marker = L.marker([44.799722,20.405278], {title: 'VEXION LOCATION'});
 marker.bindPopup("<span clas=\"map-popup\">&#9762; vexion</span>");
 marker.addTo(map);
