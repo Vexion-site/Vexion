59035c6fb05ae5b02a22a896641b36333279f04b
Editor <lordvex@keemail.me>
2021-02-06 22:27:06 +0100

    pyramid

--- main.js
+++ main.js
@@ -13,9 +13,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 // map.addLayer(Stamen_Toner);
+var pyramidicon = L.icon({
+    iconUrl: 'images/markeri/marker1.png',
+ 
 
+    iconSize:     [64, 77], // size of the icon
+   
+    iconAnchor:   [32, 37], // point of the icon which will correspond to marker's location
+  
+    popupAnchor:  [0, 38] // point from which the popup should open relative to the iconAnchor
 
-let marker = L.marker([44.799722,20.405278], {title: 'VEXION LOCATION'});
+});
+
+let marker = L.marker([44.799722,20.405278], {icon: pyramidicon});
 marker.bindPopup("<span clas=\"map-popup\">&#9762; vexion</span>");
 marker.addTo(map);
 
